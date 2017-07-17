0b729270c6658e1347b93056d50221e9e41ae2aa
nizi2734 <Nicholas.Zimmerer@lasp.colorado.edu>
2017-07-17 14:59:33 -0600

    Cleaned up client/server code

--- mitm/client.py
+++ mitm/client.py
@@ -2,34 +2,49 @@
 import socket
 import argparse
 
-# Default values
-targetHost = "127.0.0.1"
-targetPort = 9999
+def main():
+    # Variables
+    targetHost = "127.0.0.1"
+    targetPort = 9999
 
-# Parse inputs
-parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--server", type=str, default=targetHost,
-                    help="Target IPv4 host address (default {})".format(targetHost))
-parser.add_argument("-p", "--port", type=int, default=targetPort,
-                    help="Port to connect to host on (default {})".format(targetPort))
-args = parser.parse_args()
+    # Handle arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--server", type=str, default=targetHost,
+                        help="Target IPv4 host address (default {})".format(targetHost))
+    parser.add_argument("-p", "--port", type=int, default=targetPort,
+                        help="Port to connect to host on (default {})".format(targetPort))
+    args = parser.parse_args()
+
+    # Spin up client
+    run_client(args.server, args.port)
 
 
-# Create socket object
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-
-    # Set timeout
-    client.settimeout(10)
-
-    # Create connection
-    client.connect((args.server, args.port))
-
-    # Initiate handshake
-    client.send(bytes('SYN', 'utf-8'))
-    responce = client.recv(3).decode('utf-8')
-
-    # Check for correct responce
-    if responce != 'ACK':
-        print("Server did not acknowledge, shutting down connection")
-        exit(1)
+def run_client(targetHost, targetPort):
     
+    # Create socket object
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+    
+        # Set timeout
+        client.settimeout(10)
+    
+        # Create connection
+        client.connect((targetHost, targetPort))
+        print("[*] Connection with {}:{}\n".format(targetHost, targetPort))
+
+    
+        # Handshake successful
+        print("Send .quit or .exit to quit")
+        while True:
+            message = input("[SEND] ")
+            client.send(bytes(message, 'utf-8'))
+    
+            # Check message against known commands
+            if message in (".quit", ".exit"): break
+    
+        # Gracefully exit 
+        client.close()
+        print("\nConnection with {} terminated".format(targetHost))
+
+
+if __name__ == '__main__':
+    main()
--- mitm/server.py
+++ mitm/server.py
@@ -3,44 +3,45 @@ import socket
 import threading
 import argparse
 
-# Default values
-bind_ip = "0.0.0.0"
-bind_port = 9999
+def main():
+    # Default values
+    bind_ip = "0.0.0.0"
+    bind_port = 9999
 
-# Parse inputs
-parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--port", type=int, default=bind_port, help="Port to connect to host on (default{})".format(bind_port))
-args = parser.parse_args()
+    # Parse inputs
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=bind_port,
+                        help="Port to connect to host on (default{})".format(bind_port))
+    args = parser.parse_args()
 
-
-# Client-handling thread
-def handle_client(client_socket):
-    
-    # Print data from client
-    request = client_socket.recv(1024).decode('utf-8')
-    print("Received: {}".format(request))
-
-    # Return responce to client
-    client_socket.send(bytes('ACK', 'utf-8'))
-    client_socket.close()
-
-
-# Create server object
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     # Spin up server
-    server.bind((bind_ip, bind_port))
-    server.listen(1)
-    print("Listening on {}:{}".format(bind_ip, bind_port))
+    run_server(bind_ip, args.port)
 
-    # Set timeout
-    server.settimeout(10)
 
-    while True:
+def run_server(bind_ip, bind_port):
 
+    # Create server object
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        # Spin up server
+        server.bind((bind_ip, bind_port))
+        server.listen(1)
+        print("[*] Listening on {}:{}".format(bind_ip, bind_port))
+    
+        # Set timeout
+        server.settimeout(10)
+    
         # Make connection with client
         client, address = server.accept()
-        print("Connection from {}:{}".format(address[0], address[1]))
+        print("[*] Connection from {}:{}\n".format(address[0], address[1]))
     
-        # Spin up client thread
-        client_handler = threading.Thread(target=handle_client, args=(client,))
-        client_handler.start()
+        while True:
+            request = client.recv(1024).decode('utf-8')
+            if request in (".quit", ".exit"): break
+            # Check for exit command
+            print("[RECEIVED] {}".format(request))
+    
+        # Gracefully exit
+        print("\nConnection terminated")
+
+if __name__ == '__main__':
+    main()
