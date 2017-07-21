b83e2c6035def6c0e9e537fb71bf8f3bb9428be9
nizi2734 <Nicholas.Zimmerer@lasp.colorado.edu>
2017-07-21 15:21:39 -0600

    Added working cherrypy server and database

---
+++ sqli/server.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+import os, sys
+import random
+import sqlite3
+import time
+import cherrypy
+
+DB_NAME = "database.db"
+
+class InjectionDemo(object):
+    @cherrypy.expose
+    def index(self):
+        return open('index.html')
+
+@cherrypy.expose
+class DatabaseHandler(object):
+    @cherrypy.tools.accept(media='text/plain')
+    def GET(self, selectTerm):
+
+        # NOTE: Constructing SQL querys with string concatenation allows for injection!
+        queryString = "SELECT username FROM users WHERE firstname='{}';".format(selectTerm.lower())
+
+        # Connect to database
+        with sqlite3.connect(DB_NAME) as connection:
+            # Return string for index.html
+            display_string = ""
+
+            # Separate into multiple commands
+            for query in queryString.split(';'):
+
+                # Execute query
+                responce = connection.execute(query)
+                for row in responce.fetchall():
+                    for field in row:
+                        display_string += "{} ".format(field)
+                    display_string += '<br>'
+
+            # Display error message
+            if display_string == "": return "No results found"
+            return display_string
+
+
+def setup_database():
+    '''
+    Create the `users` table in database when server spins up
+    '''
+    with sqlite3.connect(DB_NAME) as connection:
+        connection.execute("CREATE TABLE users (username, firstname, lastname, password)")
+        connection.execute("INSERT INTO users VALUES ('dama2741', 'david', 'madison', 'monkey123')")
+        connection.execute("INSERT INTO users VALUES ('agnwl390', 'allen', 'mackinzie', 'password1234')")
+        connection.execute("INSERT INTO users VALUES ('stwe5528', 'steven', 'west', 'password!')")
+        connection.execute("INSERT INTO users VALUES ('rabl4950', 'raquel', 'black', 'tellmeyourname')")
+        connection.execute("INSERT INTO users VALUES ('juja1425', 'juliette', 'jabroni', 'secret')")
+        connection.execute("INSERT INTO users VALUES ('jodo3849', 'john', 'doe', 'joejoemonkeybo')")
+        connection.execute("INSERT INTO users VALUES ('besm4241', 'becky', 'smith', 'qwerty1234')")
+
+def cleanup_database():
+    '''
+    Drop the `users` table from database on server shutdown
+    '''
+    with sqlite3.connect(DB_NAME) as connection:
+        connection.execute("DROP TABLE users")
+
+
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'tools.staticdir.root': os.path.abspath(sys.path[0])
+        },
+        '/database': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+        }
+    }
+
+    cherrypy.engine.subscribe('start', setup_database)
+    cherrypy.engine.subscribe('stop', cleanup_database)
+
+    webapp = InjectionDemo()
+    webapp.database = DatabaseHandler()
+    cherrypy.quickstart(webapp, '/', conf)
