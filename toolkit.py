75045a7babd7e6277bfe5a2460ad3cef447f16e0
nizi2734 <Nicholas.Zimmerer@lasp.colorado.edu>
2017-07-24 16:44:29 -0600

    Set up basic layout for toolkit launcher

--- toolkit.py
+++ toolkit.py
@@ -45,7 +45,7 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.mainWidget)
 
         # Add Close button functionality
-        self.mainWidget.closeButton.clicked.connect(self.close)
+        self.mainWidget.close_button.clicked.connect(self.close)
 
     def closeEvent(self, event):
         # Override the default close event actions. This even runs
@@ -62,29 +62,66 @@ class MainWidget(QWidget):
 
         # Create main layout and launch buttons
         self.mainLayout = QVBoxLayout(self)
-        self.sqliButton = QPushButton("SQL injetion")
-        self.sqliButton.clicked.connect(self.launchSQLi)
-        self.eavesdroppingButton = QPushButton("Eavesdropping")
-        self.eavesdroppingButton.clicked.connect(self.launchEavesdropping)
-        self.steganographyButton = QPushButton("Steganography")
-        self.steganographyButton.clicked.connect(self.launchSteganoggraphy)
+
+        self.sqli_layout = QHBoxLayout()
+        self.sqli_label = QLabel("SQL Injection")
+        self.sqli_info_button = QPushButton("Info")
+        self.sqli_info_button.clicked.connect(self.sqli_info) 
+        self.sqli_button = QPushButton("Launch demo")
+        self.sqli_button.clicked.connect(self.launchSQLi)
+        self.sqli_layout.addWidget(self.sqli_info_button)
+        self.sqli_layout.addWidget(self.sqli_button)
+
+        self.eavesdropping_layout = QHBoxLayout()
+        self.eavesdropping_label = QLabel("Eavesdropping")
+        self.eavesdropping_info_button = QPushButton("Info")
+        self.eavesdropping_info_button.clicked.connect(self.eavesdropping_info)
+        self.eavesdropping_button = QPushButton("Launch demo")
+        self.eavesdropping_button.clicked.connect(self.launch_eavesdropping)
+        self.eavesdropping_layout.addWidget(self.eavesdropping_info_button)
+        self.eavesdropping_layout.addWidget(self.eavesdropping_button)
+
+        self.steganography_layout = QHBoxLayout()
+        self.steganography_label = QLabel("Steganography")
+        self.steganography_info_button = QPushButton("Info")
+        self.steganography_encode_button = QPushButton("Encode")
+        self.steganography_decode_button = QPushButton("Decode")
+        self.steganography_info_button.clicked.connect(self.steganography_info)
+        self.steganography_encode_button.clicked.connect(self.launch_steganoggraphy_encode)
+        self.steganography_decode_button.clicked.connect(self.launch_steganoggraphy_decode)
+        self.steganography_layout.addWidget(self.steganography_info_button)
+        self.steganography_layout.addWidget(self.steganography_encode_button)
+        self.steganography_layout.addWidget(self.steganography_decode_button)
 
         # Create close button
-        self.closeButton = QPushButton("Close")
+        self.close_button = QPushButton("Close")
 
         # Add button to bottom of page
-        self.mainLayout.addWidget(self.sqliButton, alignment=Qt.AlignHCenter)
-        self.mainLayout.addWidget(self.eavesdroppingButton, alignment=Qt.AlignHCenter)
-        self.mainLayout.addWidget(self.steganographyButton, alignment=Qt.AlignHCenter)
-        self.mainLayout.addWidget(self.closeButton, alignment=Qt.AlignBottom)
+        self.mainLayout.addWidget(self.sqli_label)
+        self.mainLayout.addLayout(self.sqli_layout)
+        self.mainLayout.addWidget(self.eavesdropping_label)
+        self.mainLayout.addLayout(self.eavesdropping_layout)
+        self.mainLayout.addWidget(self.steganography_label)
+        self.mainLayout.addLayout(self.steganography_layout)
+        self.mainLayout.addWidget(self.close_button)#, alignment=Qt.AlignBottom)
     
+    def sqli_info(self):
+        self.info = QMessageBox()
+        self.info.exec()
     def launchSQLi(self):
         pass
-    def launchEavesdropping(self):
+
+    def eavesdropping_info(self):
         pass
-    def launchSteganoggraphy(self):
-        print(os.path.join(sys.path[0], "steganograpy", "encodeGUI.py"))
-        os.system(os.path.join(sys.path[0], "steganograpy", "encodeGUI.py"))
+    def launch_eavesdropping(self):
+        pass
+
+    def steganography_info(self):
+        pass
+    def launch_steganoggraphy_encode(self):
+        os.system(os.path.join(sys.path[0], "steganography", "encodeGUI.py"))
+    def launch_steganoggraphy_decode(self):
+        os.system(os.path.join(sys.path[0], "steganography", "decodeGUI.py"))
         
 
 
